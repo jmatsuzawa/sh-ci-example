271b5c5f384adaed1f91cfc90d09d733de8b8693
Jiro Matsuzawa <matsuzawa.jr@gmail.com>
2025-02-15 17:25:17 +0900

    Fix find_duplicate_files to support macOS

--- lib/lib.sh
+++ lib/lib.sh
@@ -9,5 +9,15 @@ is_dir() {
 find_duplicate_files() {
   local dir="$1"
   echo "find duplicate files: ${dir}"
-  find "${dir}" -type f -exec md5sum {} + | sort | uniq -w32 -dD
+
+  if command -v md5 > /dev/null; then # macOS
+    temp_file=$(mktemp)
+    find "${dir}" -type f -exec md5 -r {} + > "${temp_file}"
+    sort "${temp_file}" | cut -f1 -d " " | uniq -d | while read -r hash; do
+      grep "${hash}" "${temp_file}"
+    done
+    rm "${temp_file}"
+  else
+    find "${dir}" -type f -exec md5sum {} + | sort | uniq -w32 -dD
+  fi
 }
